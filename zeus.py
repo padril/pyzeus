6faaf7acb6e4b1c64d15a047b41cf10783b83747
leo peckham <padrillium@gmail.com>
2024-02-07 12:46:43 -0500

    Genericized combinators and added more operations

--- zeus.py
+++ zeus.py
@@ -1,7 +1,7 @@
 # Copyright 2024 Leo (padril) Peckham
 
 from __future__ import annotations
-from typing import Tuple, List, Union, Optional, Set, Callable
+from typing import Tuple, List, Union, Optional, Set, Callable, TypeVar
 from subprocess import Popen, PIPE
 from fcntl import fcntl, F_GETFL, F_SETFL
 from os import O_NONBLOCK
@@ -13,6 +13,8 @@ class Value:
         self.value = value
     def __repr__(self) -> str:
         return f'Value: {self.value}'
+    def __add__(self, other: Token) -> List[Token]:
+        return [self, other]
 
 class Operator:
     value: str
@@ -20,15 +22,23 @@ class Operator:
         self.value = value
     def __repr__(self) -> str:
         return f'Operator: {self.value}'
+    def __add__(self, other: Token) -> List[Token]:
+        return [self, other]
 
 class Ident:
     value: str
     def __init__(self, value: str):
         self.value = value
+    def __add__(self, other: Token) -> List[Token]:
+        return [self, other]
 
 Token = Value | Operator | Ident
 
-CombData = Optional[Tuple[str, List[str]]]
+InBase = TypeVar('InBase')
+OutBase = TypeVar('OutBase')
+InType = List[InBase]
+OutType = List[OutBase | List[OutBase]]
+CombData = Optional[Tuple[InType, OutType]]
 CombFn = Callable[[CombData], CombData]
 
 class Combinator:
@@ -48,7 +58,9 @@ class Combinator:
         def then_cat_fn(cd: CombData) -> CombData:
             cd = other(self(cd))
             if not cd: return None
-            return (cd[0], cd[1][0:-2] + [cd[1][-2] + cd[1][-1]])
+            lcomb = cd[1][-2] if isinstance(cd[1][-2], List) else [cd[1][-2]]
+            rcomb = cd[1][-1] if isinstance(cd[1][-1], List) else [cd[1][-1]]
+            return (cd[0], cd[1][0:-2] + [lcomb + rcomb])
         return Combinator(then_cat_fn)
 
     def then_opt(self, other: Combinator) -> Combinator:
@@ -71,11 +83,13 @@ class Combinator:
             last = cd
             cd = self(cd)
             if not cd: return None
-            while cd and cd[0] != '':
+            while cd and cd[0]:
                 last = cd
                 cd = self(cd)
                 if cd and len(cd[1]) >= 2:
-                    cd = (cd[0], cd[1][0:-2] + [cd[1][-2] + cd[1][-1]])
+                    lcomb = cd[1][-2] if isinstance(cd[1][-2], List) else [cd[1][-2]]
+                    rcomb = cd[1][-1] if isinstance(cd[1][-1], List) else [cd[1][-1]]
+                    cd = (cd[0], cd[1][0:-2] + [lcomb + rcomb])
             return last if not cd else cd
         return Combinator(any_cat_fn)
 
@@ -84,7 +98,7 @@ class Combinator:
             last = cd
             cd = self(cd)
             if not cd: return None
-            while cd and cd[0] != '':
+            while cd and cd[0]:
                 last = cd
                 cd = self(cd)
             return last if not cd else cd
@@ -97,7 +111,7 @@ class Combinator:
             return None
         return Combinator(discard_fn)
 
-def cterm(s: str) -> Combinator:
+def cstr(s: str) -> Combinator:
     def cterm_fn(cd: CombData) -> CombData:
         if not cd: return None
         if len(s) > len(cd[0]): return None
@@ -106,10 +120,10 @@ def cterm(s: str) -> Combinator:
         return (cd[0][len(s):], cd[1] + [s])
     return Combinator(cterm_fn)
 
-def cset(ss: List[str]) -> Combinator:
+def cset(xs: InType) -> Combinator:
     def cset_fn(cd: CombData) -> CombData:
-        for s in ss:
-            res = cterm(s)(cd)
+        for x in xs:
+            res = cstr(x)(cd)
             if res: return res
         return None
     return Combinator(cset_fn)
@@ -129,14 +143,22 @@ def calphanum() -> Combinator:
 def lex(s: str) -> List[str]:
     res = (
             cdigit().any_cat()                  # Integers
-            .then_opt_cat(cterm('.').then_cat(cdigit().any_cat()))  # & Float
+            .then_opt_cat(cstr('.').then_cat(cdigit().any_cat()))  # & Float
             .alt(cwhite().any_cat().discard())  # Whitespace
             .alt(cset(['+', '-', '*', '/', '=']))    # Operators
-            .alt(calpha().then_opt_cat(calphanum().alt(cterm('_').any_cat())))
+            .alt(calpha().then_opt_cat(calphanum().alt(cstr('_').any_cat())))
             .any()
-           )((s, []))
-    if res: return res[1]
-    else: return []
+           )((list(s), []))
+    if res:
+        return list(
+                map(lambda xs: ''.join(xs) if isinstance(xs, List) else xs,
+                   res[1]))
+    return []
+
+
+def isfloat(s) -> bool:
+    res = s.split('.')
+    return len(res) == 2 and res[0].isdigit() and res[1].isdigit()
 
 def tokenize(command: str) -> List[Token]:
     tokens: List[Token] = []
@@ -145,34 +167,54 @@ def tokenize(command: str) -> List[Token]:
         token: Optional[Token] = None
         if lexeme == '': continue
         elif lexeme.isdigit(): token = Value(int(lexeme))
+        elif isfloat(lexeme): token = Value(float(lexeme))
         elif lexeme.isidentifier(): token = Ident(lexeme)
-        elif lexeme == '+': token = Operator('+')
+        elif lexeme in {'+', '-', '*', '/'}: token = Operator(lexeme)
         elif lexeme == '=': token = Operator('=')
         if token: tokens.append(token)
     return tokens
 
+# TODO: This whole AST thing should be done with an AST combinator
+
 class OpExpr:
     op: Operator
-    x: 'Expr'
-    y: 'Expr'
-    def __init__(self, op: Operator, x: 'Expr', y: 'Expr'):
+    x: Expr
+    y: Expr
+    def __init__(self, op: Operator, x: Expr, y: Expr):
         self.op = op
         self.x = x
         self.y = y
 
+class ArgList:
+    args: List[Ident]
+    def __init__(self, args: List[Ident]):
+        self.args = args
+
+class FuncExpr:
+    args: ArgList
+    fn: AST
+    def __init__(self, args: ArgList, fn: AST):
+        self.args = args
+        self.fn = fn
+
 Rvalue = Value | Ident
-Expr = OpExpr | Rvalue
+Expr = OpExpr | Rvalue | FuncExpr
 AST = Expr | List['AST']
 
 def parse_expr(tokens: List[Token]) -> Tuple[List[Token], Expr]:
-    if len(tokens) == 1 and \
-            isinstance(tokens[0], Rvalue):
-        return ([], tokens[0])
-    if len(tokens) >= 3 and \
-            isinstance(tokens[0], Rvalue) and \
-            isinstance(tokens[1], Operator):
-        (rem, right) = parse_expr(tokens[2:])
-        return (rem, OpExpr(tokens[1], tokens[0], right))
+    match tokens:
+        case [v] if isinstance(v, Rvalue):
+            return ([], v)
+        case [lv, op, *expr] if isinstance(lv, Rvalue) and \
+                isinstance(op, Operator):
+            (rem, rv) = parse_expr(expr)
+            return (rem, OpExpr(op, lv, rv))
+       # case [*idents, op, *expr] if all(
+       #         map(lambda i: isinstance(i, Ident), idents)) and \
+       #         op.value == '->':
+       #     (rem, fn) = parse_expr(expr)
+       #     return (rem, FuncExpr(idents, fn))
+
     raise Exception('Parse error')
 
 def parse(tokens: List[Token]) -> AST:
@@ -220,8 +262,10 @@ def generate_bash(ir: IR) -> str:
         match i[0]:
             case 'set':
                 bash += f'{i[1]}={i[2]}\n'
-            case '+':
-                bash += f'{i[1]}=$((${i[1]} {i[0]} {i[2]}))\n'
+            case '+' | '-' | '*' | '/':
+                # TODO: Implement `bc -l` for floating point values using
+                # a more advanced ast mechanism/dictionary (divf vs divi etc)
+                bash += f'{i[1]}=$(bc <<< "${i[1]} {i[0]} {i[2]}")\n'
     bash += 'echo $r0\n\0'
     return bash
 
